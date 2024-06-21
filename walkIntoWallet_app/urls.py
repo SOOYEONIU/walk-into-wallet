8f02593d5304dd28135cd32cd4adf8b491511d08
kimsooyeon <rlatndus235@gmail.com>
2024-06-21 17:21:34 +0900

    Chore : income, expense, budget 테이블에 데이터 수정, 삭제하는 뷰 작성

--- walkIntoWallet_app/urls.py
+++ walkIntoWallet_app/urls.py
@@ -3,4 +3,13 @@ from walkIntoWallet_app import views
 
 urlpatterns = [
     path('get_data/', views.get_data, name='get_data'),
+    path('add_income/', views.add_income, name='add_income'),
+    path('add_expense/', views.add_expense, name='add_expense'),
+    path('add_budget/', views.add_budget, name='add_budget'),
+    path('update_income/<int:id>/', views.update_income, name='update_income'),
+    path('update_expense/<int:id>/', views.update_expense, name='update_expense'),
+    path('update_budget/<int:id>/', views.update_budget, name='update_budget'),
+    path('delete_income/<int:id>/', views.delete_income, name='delete_income'),
+    path('delete_expense/<int:id>/', views.delete_expense, name='delete_expense'),
+    path('delete_budget/<int:id>/', views.delete_budget, name='delete_budget'),
 ]
--- walkIntoWallet_app/views.py
+++ walkIntoWallet_app/views.py
@@ -48,3 +48,76 @@ def add_expense(request):
         return JsonResponse({'message': 'Budget added successfully'})
     except (KeyError, ValueError) as e :
         return HttpResponseBadRequest(str(e))
+    
+@csrf_exempt
+@require_http_methods(["PUT"])
+def update_income(request, id):
+    try:
+        data = json.loads(request.body)
+        income = Income.objects.get(id = id)
+        income.date = data['date']
+        income.amount = data['amount']
+        income.description = data['description']
+        income.save()
+        return JsonResponse({'message': 'Income updated successfully'})
+    except (Income.DoesNotExist, KeyError, ValueError) as e :
+        return HttpResponseBadRequest(str(e))
+
+@csrf_exempt
+@require_http_methods(["PUT"])
+def update_expense(request, id):
+    try:
+        data = json.loads(request.body)
+        expense = Expense.object.get(id = id)
+        expense.date = data['date']
+        expense.amount = data['amount']
+        expense.category = data['category']
+        expense.description = data['description']
+        expense.save()
+        return JsonResponse({'message': 'Expense updated successfully'})
+    except (Expense.DoesNotExist, KeyError, ValueError) as e:
+        return HttpResponseBadRequest(str(e))
+
+@csrf_exempt
+@require_http_methods(["PUT"])
+def update_budget(request, id):
+    try:
+        data = json.loads(request.body)
+        budget = Budget.object.get(id = id)
+        budget.month = data['month']
+        budget.income_goal = data['income_goal']
+        budget.expense_limit = data['expense_limit']
+        budget.save()
+        return JsonResponse({'message': 'Budget updated successfully'})
+    except (Budget.DoesNotExist, KeyError, ValueError) as e:
+        return HttpResponseBadRequest(str(e))
+    
+@csrf_exempt
+@require_http_methods(["DELETE"])
+def delete_income(request, id):
+    try:
+        income = Income.object.get(id=id)
+        income.delete()
+        return JsonResponse({'message': 'Income deleted successfully'})
+    except Income.DoesNotExist as e:
+        return HttpResponseBadRequest(str(e))
+    
+@csrf_exempt
+@require_http_methods(["DELETE"])
+def delete_expense(request, id):
+    try:
+        expense = Expense.objects.get(id=id)
+        expense.delete()
+        return JsonResponse({'message': 'Expense deleted successfully'})
+    except Expense.DoesNotExist as e:
+        return HttpResponseBadRequest(str(e))
+    
+@csrf_exempt
+@require_http_methods(["DELETE"])
+def delete_budget(request, id):
+    try:
+        budget = Budget.object.get(id=id)
+        budget.delete()
+        return JsonResponse({'message': 'Budget deleted successfully'})
+    except Budget.DoesNotExist as e:
+        return HttpResponseBadRequest(str(e))
